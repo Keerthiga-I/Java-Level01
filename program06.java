a9163ce2e02a7a01e701dfaec103f64f2fab51e7
Keerthiga-I <keerthigairisan15@gmail.com>
2025-03-20 13:03:50 +0530

    program06.java

--- program06.java
+++ program06.java
@@ -6,26 +6,26 @@ public static void main (String[] args)
 	try{
 		
         InputStreamReader isr = new InputStreamReader(System.in);
-		BufferedReader br = new BufferedReader(isr);
+	BufferedReader br = new BufferedReader(isr);
         System.out.println("SQUARE & RECTANGLE");
-		System.out.println("-----------------------");
+	System.out.println("-----------------------");
         System.out.println("Enter the a value:");
         String s1 = br.readLine();
         int a = Integer.parseInt(s1);
         System.out.println("Enter the l value:");
         String s2=br.readLine();
-		int l = Integer.parseInt(s2);
+	int l = Integer.parseInt(s2);
         System.out.println("Enter the b value:");
-		String s3 = br.readLine();
+	String s3 = br.readLine();
         int b = Integer.parseInt(s3);
         System.out.println("Result");
         int z = a*a;
         System.out.println("Area of Square:" + z);
-		int y = 4*a;
+	int y = 4*a;
         System.out.println("Area of Perimeter:" + y);
         int w = l * b;
         System.out.println("Area of Rectangle:" + w);
-		int v = 2*(l + b);
+	int v = 2*(l + b);
         System.out.println("Area of Rectangle:" + v);
 		
 		
