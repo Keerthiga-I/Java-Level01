d5b1046b3440aa10e9ba42b6d26955f0b2b63ed4
Keerthiga-I <keerthigairisan15@gmail.com>
2025-03-20 13:02:40 +0530

    program04.java

--- program04.java
+++ program04.java
@@ -6,20 +6,20 @@ public static void main (String[] args)
 	try{
 		
         InputStreamReader isr = new InputStreamReader(System.in);
-		BufferedReader br = new BufferedReader(isr);
+	BufferedReader br = new BufferedReader(isr);
         System.out.println("GOVERNMENT OF TAMILNADU");
-		System.out.println("Electricity Bill");
-		System.out.println("-----------------------");
+	System.out.println("Electricity Bill");
+	System.out.println("-----------------------");
         System.out.println("Enter the EB-no:");
         String s1 = br.readLine();
         int a = Integer.parseInt(s1);
         System.out.println("Enter the customer name:");
         String s2=br.readLine();
         System.out.println("Enter the prev unit:");
-		String s3 = br.readLine();
+	String s3 = br.readLine();
         int x = Integer.parseInt(s3);
-		System.out.println("Enter the current unit:");
-		String s4 = br.readLine();
+	System.out.println("Enter the current unit:");
+	String s4 = br.readLine();
         int y = Integer.parseInt(s4);
         System.out.println("Result");
         int z = y-x;
