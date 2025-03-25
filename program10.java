089b208a373a0d2330a7be8344240a03fc7ee32b
Keerthiga-I <keerthigairisan15@gmail.com>
2025-03-25 15:02:55 +0530

    program10.java

--- program10.java
+++ program10.java
@@ -12,10 +12,10 @@ public static void main (String[] args)
         System.out.println("Enter a x value:");
         String s1 = br.readLine();
         int x = Integer.parseInt(s1);
-		System.out.println("Enter a y value:");
+	System.out.println("Enter a y value:");
         String s2 = br.readLine();
         int y = Integer.parseInt(s2);
-		System.out.println("Enter a z value:");
+	System.out.println("Enter a z value:");
         String s3 = br.readLine();
         int z = Integer.parseInt(s3);
         System.out.println("RESULT");
@@ -23,8 +23,8 @@ public static void main (String[] args)
         System.out.println("A:" + a);
         int b = x*y+y*z+z*x;
         System.out.println("B:" + b);
-		int c = x*x*x+x*x*y+x*z;
-		System.out.println("C:" +c);
+	int c = x*x*x+x*x*y+x*z;
+	System.out.println("C:" +c);
 	
 	}
 	catch(Exception e)
